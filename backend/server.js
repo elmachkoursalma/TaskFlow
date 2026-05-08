0ee2fb2d9bc73d1a3557b2269ca3d73ad1df5233
Iman <mohammedelkhamal@Mohammeds-MacBook-Pro.local>
2026-05-08 19:48:19 +0100

    feat: connect authentication routes to express server

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connecté'))
