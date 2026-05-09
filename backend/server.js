bc6d9b94d03cff8d63eb925f4934da40c87a338f
salma <elmachkoursalma1@gmail.com>
2026-05-09 19:41:13 +0100

    feat:connect project routes to server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authMiddleware = require('./middleware/authMiddleware');
+const projectRoutes = require('./routes/projects');
+
+
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
@@ -26,6 +29,8 @@ app.get('/api/protected', authMiddleware, (req, res) => {
     message: 'Protected route access granted',
     user: req.user
   });
+  // Ajouter après les routes auth
+app.use('/api/projects', projectRoutes);
 
 });
 app.listen(process.env.PORT, () => {
