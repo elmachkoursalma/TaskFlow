17fb58ad0ceb63de5add36e595f1634ee4f734a5
Iman <mohammedelkhamal@Mohammeds-MacBook-Pro.local>
2026-05-10 12:41:16 +0100

    fix: move project routes outside protected route

--- backend/server.js
+++ backend/server.js
@@ -2,37 +2,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authMiddleware = require('./middleware/authMiddleware');
-const projectRoutes = require('./routes/projects');
-
-
-require('dotenv').config();
-
+const projectRoutes = require('./routes/projets');
 const authRoutes = require('./routes/authRoutes');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes); // ✅ ICI dehors
+
+app.get('/', (req, res) => {
+  res.json({ message: 'TaskFlow API fonctionne ✅' });
+});
+
+app.get('/api/protected', authMiddleware, (req, res) => {
+  res.json({
+    message: 'Protected route access granted',
+    user: req.user
+  });
+}); // ✅ Fermée correctement
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connecté'))
   .catch((err) => console.error('Erreur MongoDB :', err));
 
-app.get('/', (req, res) => {
-  res.json({ message: 'TaskFlow API fonctionne ✅' });
-});
-app.get('/api/protected', authMiddleware, (req, res) => {
-
-  res.json({
-    message: 'Protected route access granted',
-    user: req.user
-  });
-  // Ajouter après les routes auth
-app.use('/api/projects', projectRoutes);
-
-});
 app.listen(process.env.PORT, () => {
   console.log(`Serveur démarré sur le port ${process.env.PORT}`);
 });
