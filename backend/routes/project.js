e809e87f85443b8c1c0da9942f49dd1305cf2c26
Iman <mohammedelkhamal@Mohammeds-MacBook-Pro.local>
2026-05-10 12:47:50 +0100

    chore: sauvegarder les dernières modifications

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Project = require('../models/Project');
+const Project = require('../models/Projets');
 const authMiddleware = require('../middleware/authMiddleware');
 
 
