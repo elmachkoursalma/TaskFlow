fc8c6c51eb0996d37d1b6ee24207bbd08613e255
Aya Amalal <aya.amalal1@etu.uae.ac.ma>
2026-05-13 22:38:13 +0100

    feat: intergrate task routes and connect to serner

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connecté'))
@@ -15,6 +20,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'TaskFlow API fonctionne ✅' });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Serveur démarré sur le port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
 });
