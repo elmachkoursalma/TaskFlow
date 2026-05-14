67fc4c551742a14d76b3ae11c0e6eddb5e6da4bc
elmachkoursalma <elmachkoursalma1@gmail.com>
2026-05-14 23:11:07 +0100

    fix :remove next() from async pre deleteOne middleware

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -21,9 +21,8 @@ const projectSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Suppression en cascade des tâches liées au projet
-projectSchema.pre('deleteOne', { document: true, query: false }, async function (next) {
+projectSchema.pre('deleteOne', { document: true, query: false }, async function () {
   await mongoose.model('Task').deleteMany({ project: this._id });
-  next();
 });
 
 module.exports = mongoose.model('Project', projectSchema);
