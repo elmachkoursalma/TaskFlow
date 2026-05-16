84d65f3a776a96c6dc86a826d977fb40b0bc9e14
elmachkoursalma <elmachkoursalma1@gmail.com>
2026-05-16 20:20:41 +0100

    fix: remove duplicate api/projects route declaration in server.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,6 @@ app.use('/api/projects', projectRoutes);
 
 app.use('/api/tasks', taskRoutes);
 
-app.use('/api/projects', projectRoutes);
-
 app.use('/api/dashboard', require('./routes/dashboard'));
 
 
