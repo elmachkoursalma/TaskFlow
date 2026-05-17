2be50128cf55038e1fc8ca44e7f15a5567f7856d
CodewithMalak <malak.elalami1@etu.uae.ac.ma>
2026-05-17 20:24:42 +0200

    feat:ajouter le schéma et le modèle des notifications

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -6,4 +6,21 @@ const notificationSchema = new mongoose.Schema({
     enum: ['tache_assignee', 'statut_modifie', 'membre_ajoute'],
     required: true
   },
-  
+  message: {
+    type: String,
+    required: true
+  },
+  //L'identifiant de l'utilisateur qui reçoit la notification.
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  //Indique si l'utilisateur a lu la notification ou pas.
+  lu: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Notification', notificationSchema);
