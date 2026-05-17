5b089dd26cb9ebe553e557da12d3c4f95bef07f8
CodewithMalak <malak.elalami1@etu.uae.ac.ma>
2026-05-17 21:01:38 +0200

    feat: enregistrer la route des membres dans le serveur

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
 app.use('/api/dashboard', require('./routes/dashboard'));
+app.use('/api/projects', require('./routes/membres'));
 app.use('/api/notifications', require('./routes/notifications'));
 
 
