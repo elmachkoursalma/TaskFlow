60e863247ac0027d6d746f97f0636d446e0f18d6
CodewithMalak <malak.elalami1@etu.uae.ac.ma>
2026-05-17 20:48:27 +0200

    feat: enregistrer la route des notifications dans le serveur

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
 app.use('/api/dashboard', require('./routes/dashboard'));
+app.use('/api/notifications', require('./routes/notifications'));
 
 
 // Test Route
